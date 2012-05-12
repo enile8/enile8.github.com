aa7a606b9ff4a2e7cab3f015f2c6ace7d8ac3198
enile8 <email@enile8.org>
2012-05-11 20:33:27 -0400

    adding fedora script page

---
+++ demos/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Welcome to my demo page!</title>
+		<meta name="description" content="enile8.org's demo page, nothing much here yet... Should be updated soon!"/>
+	</head>
+	<body>
+		<div style="text-align:center;">
+			<p>Welcome to the enile8.org demo page. There's not much here yet, but that should change very soon. Thanks for stopping by and checking it out in the mean time.</p>
+
+			<p>Perhaps you would like to check out my blog in the mean time? You can find <a href="http://enile8.org">my blog</a> here.</p>
+		</div>
+	</body>
+</html>
---
+++ demos/twentyten-horizontal/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WordPress Twenty Ten Theme with Horizontal Subnav</title>
+<link rel="stylesheet" media="all" href="http://enile8.org/demos/twentyten-horizontal/style.css" />
+
+</head>
+
+<body>
+
+<div id="wrapper" class="hfeed">
+
+	<header class="header">
+		<section id="masthead">
+			<div id="branding" role="banner">
+				<hgroup>
+										<h1 id="site-title">
+					<span>
+						<a href="http://enile8.org/blog/adding-horizontal-subnav-twenty-ten-theme" title="Theme Test" rel="home">TwentyTen Horizontal Subnav</a>
+					</span>
+
+					</h1>
+					<h2 id="site-description">WordPress Twenty Ten Theme with Horizontal Subnav</h2>
+				</hgroup>
+										<img src="http://enile8.org/demos/twentyten-horizontal/images/path.jpg" width="940" height="198" alt="" />
+								</div><!-- #branding -->
+
+<nav id="access" role="navigation">
+			  				<div class="skip-link screen-reader-text"><a href="#content" title="Skip to content">Skip to content</a></div>
+
+								<div class="menu-header"><ul id="menu-main-menu" class="menu"><li id="menu-item-205" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-205"><a href="#">Home</a></li>
+<li id="menu-item-206" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-206"><a href="#">blog</a>
+<ul class="sub-menu">
+	<li id="menu-item-209" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-209"><a href="#">front page</a>
+	<ul class="sub-menu">
+		<li id="menu-item-210" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-210"><a href="#">Subnav Link</a></li>
+	</ul>
+
+</li>
+	<li id="menu-item-211" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-211"><a href="#">Uncategorized</a></li>
+	<li id="menu-item-892" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-892"><a href="#">Level 1</a></li>
+	<li id="menu-item-893" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-893"><a href="#">Level 2</a></li>
+	<li id="menu-item-894" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-894"><a href="#">Level 3</a></li>
+</ul>
+</li>
+<li id="menu-item-207" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-207"><a href="#">About</a></li>
+
+<li id="menu-item-208" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-208"><a href="#">Contact</a></li>
+</ul></div>			</nav><!-- #access -->
+
+</nav><!-- #access -->
+		</section><!-- #masthead -->
+	</header><!-- #header -->
+	<div id="main">
+		<div id="container">
+			<div id="content" role="main">
+			
+			<article id="post-1" class="post-1 post type-post">
+			<header>
+
+				<h2 class="entry-title"><a href="http://enile8.org/blog/adding-horizontal-subnav-twenty-ten-theme" title="Permalink to Twenty Ten Theme with Horizontal Subnav" rel="bookmark">Twenty Ten Theme with Horizontal Subnav</a></h2>
+
+			</header>
+
+				<div class="entry-content">
+				<p>The WordPress Twenty Ten theme with the CSS adjusted so instead of vertical sub-navigation it now has horizontal sub-navigation. For an explaination of the underlaying code please visit the main page where I describe changing the CSS so the <a href="http://enile8.org/blog/adding-horizontal-subnav-twenty-ten-theme" title="Twenty Ten Theme with Horizontal Subnav" rel="bookmark">Twenty Ten Theme Subnav appears Horizontal</a>.
+	</div><!-- #main -->
+</div><!-- #wrapper -->
+
+</body>
+</html>
